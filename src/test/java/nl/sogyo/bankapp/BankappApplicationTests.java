c3cbf5800997089a9d360760d86e92c8ce158591
Banafsheh Abdollahi <babdollahi@sogyo.nl>
2023-05-17 14:06:49 +0200

    removed extra reports

--- src/test/java/nl/sogyo/bankapp/BankappApplicationTests.java
+++ src/test/java/nl/sogyo/bankapp/BankappApplicationTests.java
@@ -1,10 +1,7 @@
 package nl.sogyo.bankapp;
 
-import nl.sogyo.bankapp.controller.UsersController;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import static org.assertj.core.api.FactoryBasedNavigableListAssert.assertThat;
 
 @SpringBootTest
 class BankappApplicationTests {
